920e49506a24b9d7527b5ee99fee688611279167
GabrielAntonioVeiga <gabrielveiga54@gmail.com>
2025-09-30 19:26:11 -0300

    wip(Cadastro): realiza cadastro de transacaoes

--- app/src/main/java/ufpr/veiga/wallet/ui/CadastroActivity.kt
+++ app/src/main/java/ufpr/veiga/wallet/ui/CadastroActivity.kt
@@ -1,14 +1,20 @@
 package ufpr.veiga.wallet.ui
 
 import android.os.Bundle
+import android.view.View
+import android.widget.ArrayAdapter
 import android.widget.EditText
 import android.widget.Spinner
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import ufpr.veiga.wallet.R
 import ufpr.veiga.wallet.database.DBHelper
+import ufpr.veiga.wallet.model.EnClassificacaoOperacao
+import ufpr.veiga.wallet.model.Transacao
+import java.util.Objects
 
 class CadastroActivity : AppCompatActivity() {
     private lateinit var dbHelper: DBHelper;
@@ -26,9 +32,36 @@ class CadastroActivity : AppCompatActivity() {
         dbHelper = DBHelper(this);
 
         val spinner = findViewById<Spinner>(R.id.spinner)
+
+        val opcoes = arrayOf("Selecione", "Crédito", "Débito")
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, opcoes)
+        spinner.adapter = adapter
+    }
+
+    fun salvar(view: View) {
+        val spinner = findViewById<Spinner>(R.id.spinner)
         val descricao = findViewById<EditText>(R.id.editTextText)
         val valor = findViewById<EditText>(R.id.editTextNumberDecimal)
 
 
+        val opcaoSelecionado = spinner.selectedItem.toString()
+        val descricaoText = descricao.text.toString()
+        val valorDouble = valor.text.toString().toDoubleOrNull()
+
+        if (opcaoSelecionado == "Selecione" || descricaoText.isBlank() || valorDouble == null) {
+            Toast.makeText(this, "Preencha todos os campos corretamente!", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        val tipo = if (opcaoSelecionado == "Crédito") EnClassificacaoOperacao.CREDITO else EnClassificacaoOperacao.DEBITO
+
+        dbHelper.insertTransacao(
+            Transacao(
+                tipo = tipo,
+                descricao = descricaoText,
+                valor = valorDouble
+            )
+        )
+        Toast.makeText(this, "Transação salva!", Toast.LENGTH_SHORT).show()
     }
 }
